fe72da3be157f654d2f470c9d1b78b848beb9646
Mr-Tetin <raimundo.porto5566@gmail.com>
2026-07-10 18:10:38 -0300

    adicionei mais projetos e tirei projetos.json

--- js/projetos.js
+++ js/projetos.js
@@ -10,5 +10,16 @@ let projetos = [
         nome: "HUB de Rede (PAYBACK)",
         descricao: "Um pequeno HUB de funções em Python relacionados a rede que pode ser usado para testes (também foi usado como aprendizado)",
         link: "https://github.com/Mr-Tetin/PayBack"
-    }
+    },
+    {
+        nome: "Campo Minado (C)",
+        descricao: "Um pequeno jogo de campo minado em python que fiz com um amigo.",
+        link: "https://github.com/Mr-Tetin/Campo_Minado"
+    },
+    {
+        nome: "Caça Níquel (Python)",
+        descricao: "Um sistema de caça níquel em que fiz para estudar a teoria dos grandes números e como cassinos são desonestos.",
+        link: "https://github.com/Mr-Tetin/Caca_niquel"
+    },
+    
 ]
